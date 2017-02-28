c99965907ade0cd7b37e97657d45076ec231ffca
Alex Radevich <virtuain98@gmail.com>
2017-03-01 01:36:17 +0300

    Adding shapes in list

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserShapes;
 
 namespace PaintWPF
 {
@@ -24,6 +25,11 @@ namespace PaintWPF
         {
             InitializeComponent();
 
+            var shapeList = new ShapeList();
+            foreach (UserShapes.Shape shape in shapeList)
+            {
+                shape.draw();
+            }
         }
     }
 }
--- Shapes/ShapeList.cs
+++ Shapes/ShapeList.cs
@@ -1,30 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace UserShapes
 {
-    class ShapeList
+    class ShapeList : List<Shape>
     {
-        private List<Shape> shapeList;
-
         public ShapeList()
         {
             try
             {
-                shapeList.Add(new Ellipse(new Point(20, 20), new Point(100, 140)));
-                shapeList.Add(new Circle(new Point(150, 20), new Point(230, 140)));          
-                shapeList.Add(new RightTriangle(new Point(20, 200), new Point(100, 320)));
-                shapeList.Add(new IsoscalesTriangle(new Point(150, 200), new Point(230, 320)));            
-                shapeList.Add(new Rectangle(new Point(20, 380), new Point(100, 500)));
-                shapeList.Add(new Square(new Point(150, 380), new Point(230, 500)));               
+                Add(new Ellipse(new Point(20, 20), new Point(100, 140)));
+                Add(new Circle(new Point(150, 20), new Point(230, 140)));
+                Add(new RightTriangle(new Point(20, 200), new Point(100, 320)));
+                Add(new IsoscalesTriangle(new Point(150, 200), new Point(230, 320)));
+                Add(new Rectangle(new Point(20, 380), new Point(100, 500)));
+                Add(new Square(new Point(150, 380), new Point(230, 500)));               
             }
             catch
             {
                 throw;
             }
-        }
+        } 
     }
 }
