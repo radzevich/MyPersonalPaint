8dec281d4db422f88f0a6e7826086e730a0afb7a
Alex Radevich <virtuain98@gmail.com>
2017-03-18 00:48:49 +0300

    Meta

---
+++ Meta/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintWPF.meta
+{
+    abstract class Shape
+    {
+        public virtual string IconImageName { get { return (GetType().ToString() + ".png"); } }
+        public abstract bool RightShape { get; }
+
+        /*public Shape(string iconImageName, bool rightShape)
+        {
+            if (iconImageName == null)
+            {
+                throw new ArgumentNullException();
+            }
+            this.iconImageName = iconImageName;
+            this.rightShape = rightShape;
+        }*/
+    }
+}
