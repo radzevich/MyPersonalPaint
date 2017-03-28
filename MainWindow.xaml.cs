f3ab60732a978558bd63907b789e9c7c4af8c248
Alex Radevich <virtuain98@gmail.com>
2017-03-28 14:20:00 +0300

    Star drawing method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,66 +13,13 @@ namespace PaintWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DrawingContext dc;
-
-        public DrawingContext Dc
-        {
-            get { return this.dc; }
-            set
-            {
-                dc = value;
-                
-            }
-        }
-
         public MainWindow()
         {
             InitializeComponent();
 
-            var shapeDrawer = new RightTriangleDrawer();
-            Stroke rect = shapeDrawer.draw(new Point(50, 500), new Point(200, 200));
-
-            DrawingAttributes attribs = new DrawingAttributes();
-            //attribs.Color = ;//Colors.LimeGreen;
-            attribs.Height = 5.0;
-            attribs.Width = 5.0;
-            attribs.FitToCurve = false;
-            //drawingBox.IsEnabled = false;
-
-            rect.DrawingAttributes = attribs;
-
-            drawingBox.Strokes.Add(rect);
-
-            //drawingBox.Strokes.Clear();
-            /*var circle = new Ellipse();
-            var firstPoint = new Point(00, 100);
-            circle.Width = 5;
-            circle.Height = 5;
-            circle.Stroke = Brushes.Black;
-            circle.Width = 65;
-            circle.Height = 70;*/
-            //InkCanvas.SetTop(circle, firstPoint.Y);
-            //InkCanvas.SetLeft(circle, firstPoint.X);
-            //drawingBox.Strokes.Draw()
-
-            this.InvalidateVisual();
-            Pen myPen = new Pen(Brushes.Black, 3);
-            if (dc != null)
-            {
-                dc.DrawLine(myPen, new Point(100, 100), new Point(500, 500));
-                drawingBox.Strokes.Draw(dc);
-            }
-        }
-
-        protected override void OnRender(DrawingContext dc)
-        {
-            base.OnRender(dc);
-
-            Pen myPen = new Pen(Brushes.Black, 3);
-            dc.DrawLine(myPen, new Point(100, 100), new Point(500, 500));
-            drawingBox.Strokes.Draw(dc);
+            var drawer = new StarDrawer();
+            var stroke = drawer.draw(new Point(100, 100), new Point(200, 200));
+            drawingBox.Strokes.Add(stroke);
         }
     }
 }
-
-
